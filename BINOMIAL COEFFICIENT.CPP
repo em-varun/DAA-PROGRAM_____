af863a643f8be8bf4da142de34938971263355fe
em-varun <131890144+em-varun@users.noreply.github.com>
2023-08-28 15:04:38 +0530

    Add files via upload

---
+++ BINOMIAL COEFFICIENT.CPP
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int binomialCoefficients(int n, int k) {
+   if (k == 0 || k == n)
+   return 1;
+   return binomialCoefficients(n - 1, k - 1) + binomialCoefficients(n - 1, k);
+}
+int main() {
+   int n=8 , k=5;
+   cout<<"The value of C("<<n<<", "<<k<<") is "<<binomialCoefficients(n, k);
+   return 0;
+}
